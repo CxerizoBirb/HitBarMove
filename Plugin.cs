a2b7004a5ed353ab9015d97395d65945909dbf50
Mizhi <117398705+wasoKewi@users.noreply.github.com>
2024-11-10 13:56:45 +0000

    Added toggles for individual colours Made height slider Large

--- Plugin.cs
+++ Plugin.cs
@@ -21,6 +21,14 @@ public class Plugin : BaseUnityPlugin
     private static ConfigEntry<int> barY;
     private static ConfigEntry<bool> enableBacking;
     
+    // private static ConfigEntry<bool> enableColourBar;
+    
+    private static ConfigEntry<bool> enableAwful;
+    private static ConfigEntry<bool> enableOkay;
+    private static ConfigEntry<bool> enableGreat;
+    private static ConfigEntry<bool> enablePerfect;
+    private static ConfigEntry<bool> enablePerfectPlus;
+    
     private void Awake()
     {
         // Plugin startup logic
@@ -28,8 +36,16 @@ public class Plugin : BaseUnityPlugin
         Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} exists!");
         
         //Sets saved var ["name of section (default "general")", "internal key", Default Value, "description of the value"]
-        barY = Config.Bind("general", "barY", 300, "Sets the bar Y position"); 
-        enableBacking = Config.Bind("general", "enableBacking", true, "Toggles the black border around the timing bar");
+        barY              = Config.Bind("general", "barY", 300, "Sets the bar Y position"); 
+        enableBacking     = Config.Bind("general", "enableBacking", true, "Toggles the black border around the timing bar");
+        // enableColourBar   = Config.Bind("general", "enableColourBar", true, "Toggles the colours on the timing bar");
+        
+        enableAwful       = Config.Bind("general", "enableAwful", true, "Toggles Awful colour rectangles on the timing bar");
+        enableOkay        = Config.Bind("general", "enableOkay", true, "Toggles Okay colour rectangles on the timing bar");
+        enableGreat       = Config.Bind("general", "enableGreat", true, "Toggles Great colour rectangles on the timing bar");
+        enablePerfect     = Config.Bind("general", "enablePerfect", true, "Toggles Perfect colour rectangles on the timing bar");
+        enablePerfectPlus = Config.Bind("general", "enablePerfectPlus", true, "Toggles PerfectPlus colour rectangles on the timing bar");
+        
         
         //patches typeof(function name) every frame
         Harmony.CreateAndPatchAll(typeof(patches));
@@ -37,11 +53,20 @@ public class Plugin : BaseUnityPlugin
         //["translationkey", "Visual name in game"]
         TranslationHelper.AddTranslation("HBM_BarHeaderName", "Hit Bar Move");
         TranslationHelper.AddTranslation("HBM_BarYNumName", "Height");
-        TranslationHelper.AddTranslation("HBM_ToggleBGName", "Enable Background");
+        TranslationHelper.AddTranslation("HBM_ToggleBGName", "Enable Bar Border");
+        // TranslationHelper.AddTranslation("HBM_ToggleColourName", "Enable Bar Colours");
+        
+        TranslationHelper.AddTranslation("HBM_ToggleAwfulName", "Enable Early/Late Colours");
+        TranslationHelper.AddTranslation("HBM_ToggleOkayName", "Enable Okay Colours");
+        TranslationHelper.AddTranslation("HBM_ToggleGreatName", "Enable Great Colours");
+        TranslationHelper.AddTranslation("HBM_TogglePerfectName", "Enable Perfect Colours");
+        TranslationHelper.AddTranslation("HBM_TogglePerfectPlusName", "Enable Perfect+ Colours");
+        
+        
         
         UIHelper.RegisterGroupInQuickModSettings(parent =>
         {
-            var bargroup = UIHelper.CreateGroup(parent, "Bargroup");
+            var bargroup = UIHelper.CreateGroup(parent, "BarGroup");
             
             UIHelper.CreateSectionHeader(
                 bargroup,
@@ -50,13 +75,13 @@ public class Plugin : BaseUnityPlugin
                 false //required
             );
             
-            UIHelper.CreateSmallMultiChoiceButton(
+            UIHelper.CreateLargeMultiChoiceButton(
                 bargroup,
                 "BarYNum",
                 "HBM_BarYNumName",
                 barY.Value, // default Value
                 v => barY.Value = v, // sets barY to v
-                () => new IntRange(0, 450), //sets the int range for v
+                () => new IntRange(-200, 450), //sets the int range for v
                 v => v.ToString() //text shown on the thing
             );
 
@@ -67,6 +92,57 @@ public class Plugin : BaseUnityPlugin
                 enableBacking.Value,
                 v => enableBacking.Value = v
             );
+
+            // UIHelper.CreateSmallToggle(
+            //     bargroup,
+            //     "ToggleColours",
+            //     "HBM_ToggleColourName",
+            //     enableColourBar.Value,
+            //     v => enableColourBar.Value = v
+            // );
+            
+            UIHelper.CreateSmallToggle(
+                bargroup,
+                "ToggleAwful",
+                "HBM_ToggleAwfulName",
+                enableAwful.Value,
+                v => enableAwful.Value = v
+            );
+            
+            UIHelper.CreateSmallToggle(
+                bargroup,
+                "ToggleOkay",
+                "HBM_ToggleOkayName",
+                enableOkay.Value,
+                v => enableOkay.Value = v
+            );
+            
+            UIHelper.CreateSmallToggle(
+                bargroup,
+                "ToggleGreat",
+                "HBM_ToggleGreatName",
+                enableGreat.Value,
+                v => enableGreat.Value = v
+            );
+            
+            UIHelper.CreateSmallToggle(
+                bargroup,
+                "TogglePerfect",
+                "HBM_TogglePerfectName",
+                enablePerfect.Value,
+                v => enablePerfect.Value = v
+            );
+
+            UIHelper.CreateSmallToggle(
+                bargroup,
+                "TogglePerfectPlus",
+                "HBM_TogglePerfectPlusName",
+                enablePerfectPlus.Value,
+                v => enablePerfectPlus.Value = v
+            );
+
+
+            
         });
     }
 
@@ -82,6 +158,33 @@ public class Plugin : BaseUnityPlugin
             
             parent.Find("Accuracy Bar Backing").gameObject.SetActive(enableBacking.Value);
             
+            // parent.Find("Rectangles").gameObject.SetActive(enableColourBar.Value);
+            // parent.Find("Caps").gameObject.SetActive(enableColourBar.Value);
+            
+            //Awful Enabling
+            parent.Find("Rectangles").GetChild(0).gameObject.SetActive(enableAwful.Value);
+            parent.Find("Rectangles").GetChild(1).gameObject.SetActive(enableAwful.Value);
+            parent.Find("Caps").gameObject.SetActive(enableAwful.Value);
+            
+            //Okay Enabling
+            parent.Find("Rectangles").GetChild(2).gameObject.SetActive(enableOkay.Value);
+            parent.Find("Rectangles").GetChild(3).gameObject.SetActive(enableOkay.Value);
+            
+            //Great Enabling
+            parent.Find("Rectangles").GetChild(4).gameObject.SetActive(enableGreat.Value);
+            parent.Find("Rectangles").GetChild(5).gameObject.SetActive(enableGreat.Value);
+            
+            //Perfect Enabling
+            parent.Find("Rectangles").GetChild(6).gameObject.SetActive(enablePerfect.Value);
+            parent.Find("Rectangles").GetChild(7).gameObject.SetActive(enablePerfect.Value);
+            
+            //PerfectPlus Enabling
+            parent.Find("Rectangles").GetChild(8).gameObject.SetActive(enablePerfectPlus.Value);
+            
+            
         }
     }
 }
+//Todo: Allow Changing Pallete of the timing bar
+//Todo: Move the quick settings to the mod menu settings
+//Todo: Have the "Enable Bar Colours" have a dropdown when enabled to disable and enable individual components
