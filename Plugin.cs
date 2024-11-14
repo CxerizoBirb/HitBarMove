e2d6ba12206dfa8d5a5f60d0d54657d1a7daf1e5
Mizhi <117398705+wasoKewi@users.noreply.github.com>
2024-11-14 09:23:16 +0000

    Added: - Mod Settings Bar - Scale
    Changed:
    - Moved toggles to mod settings page

--- Plugin.cs
+++ Plugin.cs
@@ -1,4 +1,5 @@
-﻿using BepInEx;
+﻿using System;
+using BepInEx;
 using BepInEx.Configuration;
 using BepInEx.Logging;
 using HarmonyLib;
@@ -29,6 +30,12 @@ public class Plugin : BaseUnityPlugin
     private static ConfigEntry<bool> enablePerfect;
     private static ConfigEntry<bool> enablePerfectPlus;
     
+    private static ConfigEntry<int> barXScale;
+    private static ConfigEntry<int> barYScale;
+    
+    private static float barYScaleFloat;
+    private static float barXScaleFloat;
+    
     private void Awake()
     {
         // Plugin startup logic
@@ -37,6 +44,11 @@ public class Plugin : BaseUnityPlugin
         
         //Sets saved var ["name of section (default "general")", "internal key", Default Value, "description of the value"]
         barY              = Config.Bind("general", "barY", 300, "Sets the bar Y position"); 
+        
+        barXScale         = Config.Bind("general", "barXScale", 100, "Sets the bar X scale");
+        barYScale         = Config.Bind("general", "barYScale", 100, "Sets the bar Y scale");
+
+        
         enableBacking     = Config.Bind("general", "enableBacking", true, "Toggles the black border around the timing bar");
         // enableColourBar   = Config.Bind("general", "enableColourBar", true, "Toggles the colours on the timing bar");
         
@@ -51,16 +63,33 @@ public class Plugin : BaseUnityPlugin
         Harmony.CreateAndPatchAll(typeof(patches));
         
         //["translationkey", "Visual name in game"]
+        
+        //Quick Mod Settings Page
         TranslationHelper.AddTranslation("HBM_BarHeaderName", "Hit Bar Move");
         TranslationHelper.AddTranslation("HBM_BarYNumName", "Height");
+        
+        TranslationHelper.AddTranslation("HBM_BarXScaleName", "X Scale");
+        TranslationHelper.AddTranslation("HBM_BarYScaleName", "Y Scale");
+        
         TranslationHelper.AddTranslation("HBM_ToggleBGName", "Enable Bar Border");
         // TranslationHelper.AddTranslation("HBM_ToggleColourName", "Enable Bar Colours");
         
+        //Mod Settings Page
+        TranslationHelper.AddTranslation("HBM_ModPageName", "HitBarMove");
+        TranslationHelper.AddTranslation("HBM_ModPageSectionName", "HitBarMove");
+        
+        
+        TranslationHelper.AddTranslation("HBM_GeneralSettingsName", "General Settings");
+        TranslationHelper.AddTranslation("HBM_ColourSectionName", "Timing Sections");
         TranslationHelper.AddTranslation("HBM_ToggleAwfulName", "Enable Early/Late Colours");
         TranslationHelper.AddTranslation("HBM_ToggleOkayName", "Enable Okay Colours");
         TranslationHelper.AddTranslation("HBM_ToggleGreatName", "Enable Great Colours");
         TranslationHelper.AddTranslation("HBM_TogglePerfectName", "Enable Perfect Colours");
         TranslationHelper.AddTranslation("HBM_TogglePerfectPlusName", "Enable Perfect+ Colours");
+        
+        
+        
+        
         
         
         
@@ -84,15 +113,77 @@ public class Plugin : BaseUnityPlugin
                 () => new IntRange(-200, 450), //sets the int range for v
                 v => v.ToString() //text shown on the thing
             );
-
-            UIHelper.CreateSmallToggle(
+            
+            UIHelper.CreateSmallMultiChoiceButton(
                 bargroup,
+                "BarXScale",
+                "HBM_BarXScaleName",
+                barXScale.Value,
+                v => barXScale.Value = v,
+                () => new IntRange(0, 300),
+                v => v.ToString()
+            );
+            
+            UIHelper.CreateSmallMultiChoiceButton(
+                bargroup,
+                "BarYScale",
+                "HBM_BarYScaleName",
+                barYScale.Value,
+                v => barYScale.Value = v,
+                () => new IntRange(0, 300),
+                v => v.ToString()
+            );
+        });
+        
+        var barModPage = UIHelper.CreateCustomPage("ModSettings");
+        barModPage.OnPageLoad += modPageTransform =>
+        {
+            var modpage = UIHelper.CreateGroup(modPageTransform, "BarGroup");
+            UIHelper.CreateSectionHeader(
+                modpage,
+                "ModPageSection",
+                "HBM_ModPageSectionName",
+                false
+            );
+            
+            UIHelper.CreateLargeMultiChoiceButton(
+                modpage,
+                "BarYNum",
+                "HBM_BarYNumName",
+                barY.Value, // default Value
+                v => barY.Value = v, // sets barY to v
+                () => new IntRange(-200, 450), //sets the int range for v
+                v => v.ToString() //text shown on the thing
+            );
+            
+            UIHelper.CreateSmallMultiChoiceButton(
+                modpage,
+                "BarXScale",
+                "HBM_BarXScaleName",
+                barXScale.Value,
+                v => barXScale.Value = v,
+                () => new IntRange(0, 300),
+                v => v.ToString()
+            );
+            
+            UIHelper.CreateSmallMultiChoiceButton(
+                modpage,
+                "BarYScale",
+                "HBM_BarYScaleName",
+                barYScale.Value,
+                v => barYScale.Value = v,
+                () => new IntRange(0, 300),
+                v => v.ToString()
+            );
+            
+            UIHelper.CreateSmallToggle(
+                modpage,
                 "ToggleBG",
                 "HBM_ToggleBGName",
                 enableBacking.Value,
                 v => enableBacking.Value = v
             );
-
+            
             // UIHelper.CreateSmallToggle(
             //     bargroup,
             //     "ToggleColours",
@@ -101,32 +192,39 @@ public class Plugin : BaseUnityPlugin
             //     v => enableColourBar.Value = v
             // );
             
+            UIHelper.CreateSectionHeader(
+                modpage,
+                "ModPageSection",
+                "HBM_ColourSectionName",
+                true
+            );
+                
             UIHelper.CreateSmallToggle(
-                bargroup,
+                modpage,
                 "ToggleAwful",
                 "HBM_ToggleAwfulName",
                 enableAwful.Value,
                 v => enableAwful.Value = v
             );
-            
+
             UIHelper.CreateSmallToggle(
-                bargroup,
+                modpage,
                 "ToggleOkay",
                 "HBM_ToggleOkayName",
                 enableOkay.Value,
                 v => enableOkay.Value = v
             );
-            
+
             UIHelper.CreateSmallToggle(
-                bargroup,
+                modpage,
                 "ToggleGreat",
                 "HBM_ToggleGreatName",
                 enableGreat.Value,
                 v => enableGreat.Value = v
             );
-            
+
             UIHelper.CreateSmallToggle(
-                bargroup,
+                modpage,
                 "TogglePerfect",
                 "HBM_TogglePerfectName",
                 enablePerfect.Value,
@@ -134,16 +232,15 @@ public class Plugin : BaseUnityPlugin
             );
 
             UIHelper.CreateSmallToggle(
-                bargroup,
+                modpage,
                 "TogglePerfectPlus",
                 "HBM_TogglePerfectPlusName",
                 enablePerfectPlus.Value,
                 v => enablePerfectPlus.Value = v
             );
-
-
-            
-        });
+        };
+        
+        UIHelper.RegisterMenuInModSettingsRoot("HBM_ModPageName", barModPage);
     }
 
     public class patches // does this every frame
@@ -152,9 +249,13 @@ public class Plugin : BaseUnityPlugin
         [HarmonyPrefix] //does it before (pre-fix)
         static void meow(HudTimingAccuracyBar __instance)
         {
+            barYScaleFloat    = barYScale.Value;
+            barXScaleFloat    = barXScale.Value;
             
             var parent = __instance._rectTransform.parent;
             parent.localPosition = new Vector3(__instance._rectTransform.localPosition.x, barY.Value, -2);
+            parent.localScale = new Vector3(barXScaleFloat/100, barYScaleFloat/100, 1);
+            
             
             parent.Find("Accuracy Bar Backing").gameObject.SetActive(enableBacking.Value);
             
@@ -180,11 +281,8 @@ public class Plugin : BaseUnityPlugin
             
             //PerfectPlus Enabling
             parent.Find("Rectangles").GetChild(8).gameObject.SetActive(enablePerfectPlus.Value);
-            
-            
         }
     }
 }
 //Todo: Allow Changing Pallete of the timing bar
-//Todo: Move the quick settings to the mod menu settings
-//Todo: Have the "Enable Bar Colours" have a dropdown when enabled to disable and enable individual components
+//Todo: Have the "Enable Bar Colours" have a dropdown when enabled to disable and enable individual components (Impossible)
